2fd4c17caa47170c8d9e00bd406dbdd90176fa93
marialuizab11 <marialuizabsantos0@gmail.com>
2026-02-25 20:32:50 -0300

    feat: adiciona rota no app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './components/layout/layout';
+
+const Home = () => <div className="p-10 text-2xl font-bold">Bem-vindo à Homepage do Horizon!</div>;
+const Cadastro = () => <div className="p-10 text-2xl font-bold">Tela de Cadastro de Usuário</div>;
+
 function App() {
-  return <h1>Sistema de Hospedagem</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          
+          <Route index element={<Home />} />
+          
+          <Route path="cadastro" element={<Cadastro />} />
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
